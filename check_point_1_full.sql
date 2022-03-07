fc2543aecd8342ac13accbe8abe739cc5d985479
MustofAhmed41 <mustofahmed24@gmail.com>
2022-03-07 08:31:03 +0600

    edited output

--- check_point_1_full.sql
+++ check_point_1_full.sql
@@ -1,3 +1,7 @@
+set lines 300
+set trimout on
+set tab off
+
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
@@ -13,68 +17,68 @@ CREATE TABLE RAW_DATA (
 	numerical_col_1 float NULL, 	
 	numerical_col_2 int NULL, 
 	categorical_col_1 varchar2(30) NULL,
+	categorical_col_2 varchar2(30) NULL,
 	PRIMARY KEY(id)
 );
 
 
 
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (1,632, 'Gray', 43);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (2,474, 'White', 115);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (3,593, 'Black', 3);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (4,687, 'Brown', 57);
-INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2) VALUES (5,543, 37);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (6,527, 'Red', 114);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (7,330, 'Blue', 14);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (8,589, 'Purple', 169);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (9,665, 'Burgundy', 42);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (10,372, 'Burgundy', 64);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (11,368, 'Purple', 92);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (12,307, 'Red', 49);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (13,316, 'Brown', 156);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (14,495, 'Gray', 54);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (15,377, 'White', 48);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (16,425, 'Brown', 145);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (17,646, 'Black', 185);
-INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2) VALUES (18, 'Gray', 90);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (19,528, 'Black', 124);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (20,688, 'Gray', 9);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (21,665, 'Brown', 165);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (22,519, 'Red', 149);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (23,481, 'Silver', 139);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (24,470, 'Maroon', 21);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (25,445, 'Blue', 100);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (26,526, 'Black', 153);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (27,680, 'Purple', 106);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (28,670, 'Gray', 134);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (29,318, 'Brown', 22);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (30,344, 'White', 1);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (31,365, 'Burgundy', 79);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (32,502, 'Blue', 190);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (33,368, 'Burgundy', 149);
-INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2) VALUES (34, 'Silver', 33);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (35,365, 'Gray', 197);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (36,432, 'Red', 172);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (37,460, 'White', 174);
-INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2) VALUES (38,403, 118);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (39,481, 'Silver', 200);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (40,322, 'Purple', 185);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (41,408, 'Burgundy', 23);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (42,642, 'Brown', 36);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (43,700, 'Maroon', 189);
-INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2) VALUES (44,480, 153);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (45,395, 'Gray', 34);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (46,376, 'Maroon', 73);
-INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2) VALUES (47, 'White', 70);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (48,548, 'Black', 100);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (49,557, 'Blue', 163);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (50,529, 'Red', 142);
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (1,632, 'Gray', 43, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (2,474, 'White', 115, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (3,593, 'Black', 3, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (4,687, 'Brown', 57, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (5,543, 'Brown', 0, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2, categorical_col_2) VALUES (6,527, 114, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (7,330, 'Blue', 14, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (8,589, 'Purple', 169, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (9,665, 'Burgundy', 42, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (10,372, 'Burgundy', 64, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (11,368, 'Purple', 92, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (12,307, 'Red', 49, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (13,316, 'Brown', 156, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (14,495, 'Gray', 54, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (15,377, 'White', 48, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (16,425, 'Brown', 145, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (17,646, 'Black', 185, 'Nissan');
+INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (18, 'Gray', 90, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (19,528, 'Black', 124,  'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (20,688, 'Gray', 9, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (21,665, 'Brown', 165, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (22,519, 'Red', 149, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2, categorical_col_2) VALUES (23,481, 139, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (24,470, 'Maroon', 21, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (25,445, 'Blue', 100, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (26,526, 'Black', 153, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (27,680, 'Purple', 106, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (28,670, 'Gray', 134, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (29,318, 'Brown', 22, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (30,344, 'White', 1, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (31,365, 'Burgundy', 79, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (32,502, 'Blue', 190, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (33,368, 'Burgundy', 149, 'Nissan');
+INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (34, 'Silver', 33, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (35,365, 'Gray', 197, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (36,432, 'Red', 172, 'Lexus');
+INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2, categorical_col_2) VALUES (37,460, 174, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1,numerical_col_2, categorical_col_2) VALUES (38,403, 'Silver', 118, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (39,481, 'Silver', 42, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (40,322, 'Purple', 185, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (41,408, 'Burgundy', 23, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (42,642, 'Brown', 36, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (43,700, 'Maroon', 189, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1,numerical_col_2, categorical_col_2) VALUES (44,480, 'White',153, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (45,395, 'Gray', 34, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (46,376, 'Maroon', 73, 'Nissan');
+INSERT INTO RAW_DATA(id, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (47, 'White', 70, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (48,548, 'Black', 100, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, numerical_col_2, categorical_col_2) VALUES (49,557, 163, 'Toyota');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (50,529, 'Red', 142, 'Toyota');
 
 --inserting outlier values
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (51,950, 'Silver', 165);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (52,1000, 'Purple', 139);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (53,25, 'Black', 97);
-INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2) VALUES (54,40, 'Gray', 42);
-
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (51,950, 'Silver', 199, 'Nissan');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (52,1000, 'Purple', 0, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (53,25, 'Black', 97, 'Honda');
+INSERT INTO RAW_DATA(id, numerical_col_1, categorical_col_1, numerical_col_2, categorical_col_2) VALUES (54,40, 'Gray', 200, 'Toyota');
 
 
 SELECT * FROM raw_data WHERE numerical_col_1 IS NULL;  -- PRINT NULL VALUES.
@@ -100,12 +104,16 @@ END;
 commit;
 */
 
+SELECT id, numerical_col_1 FROM raw_data;
+
 
 DECLARE
 	mean_val float := 0;
 	mean_counter INT := 0;
 	
 BEGIN
+
+	DBMS_OUTPUT.PUT_LINE('Finding Missing Values for Numerical Feature');
 	
 	FOR r IN (SELECT numerical_col_1 FROM RAW_DATA) 
 		LOOP
@@ -132,7 +140,9 @@ END;
 
 commit;
 
-select * from raw_data;
+SELECT id, numerical_col_1 FROM raw_data;
+
+SELECT id, categorical_col_1 FROM raw_data;
 
 
 DECLARE
@@ -141,7 +151,9 @@ DECLARE
 	num_of_occurences INT;	
 	
 BEGIN
-
+	
+	DBMS_OUTPUT.PUT_LINE('Finding Missing Values For Categorical Features');
+	
 	select * into most_frequent_val,num_of_occurences from (select categorical_col_1, count(*) from raw_data 
 	group by categorical_col_1 order by count(*) desc) where rownum = 1;
 	
@@ -156,14 +168,13 @@ BEGIN
 		END IF;
 	END LOOP;
 	
-	
 END;
 /
 
 commit;
 
 
-select * from raw_data;
+SELECT id, categorical_col_1 FROM raw_data;
 
 
 
@@ -176,6 +187,7 @@ SELECT R.id, R.numerical_col_1
 FROM raw_data R ORDER BY R.numerical_col_1;
 
 
+SELECT id, numerical_col_1 FROM raw_data;
 
 DECLARE 
 	lower_quartile_index INT := 0;
@@ -188,6 +200,9 @@ DECLARE
 	lower_outlier_boundary FLOAT := 0;
 	interquartile_range FLOAT := 0;
 BEGIN 
+
+	DBMS_OUTPUT.PUT_LINE('Finding Outliers');
+	
 	SELECT COUNT(*) INTO size_of_data FROM raw_data;
 	
 	lower_quartile_index := (size_of_data) * (1/4);
@@ -216,24 +231,36 @@ BEGIN
 	upper_outlier_boundary := upper_quartile_value + interquartile_range * 1.5;
 	DBMS_OUTPUT.PUT_LINE('Upper Outlier Boundary : ' || upper_outlier_boundary);
 	
-	
-	
 	FOR r IN (SELECT id,numerical_col_1 FROM RAW_DATA) 
-		LOOP
+	LOOP
 		IF r.numerical_col_1 > upper_outlier_boundary or r.numerical_col_1 < lower_outlier_boundary then 
-			DBMS_OUTPUT.PUT_LINE('Detected Outlier : ' || r.numerical_col_1);
-			DELETE FROM raw_data WHERE id = r.id;
+			DBMS_OUTPUT.PUT_LINE('Detected Outlier : ' || r.numerical_col_1);		
 		END IF;
 	END LOOP;
 	
 END;
 /
 
+/*
+
+-----      REMOVING  OUTLIERS, ADD ABOVE IF REQUIRED
+
+FOR r IN (SELECT id,numerical_col_1 FROM RAW_DATA) 
+	LOOP
+	IF r.numerical_col_1 > upper_outlier_boundary or r.numerical_col_1 < lower_outlier_boundary then 
+		DBMS_OUTPUT.PUT_LINE('Detected Outlier : ' || r.numerical_col_1);
+		DELETE FROM raw_data WHERE id = r.id;
+	END IF;
+END LOOP;
+	
+
+*/
+
+
+
 commit;
 
 
-
-/*    STANDARDIZING VALUES     */
 
 
 
@@ -242,6 +269,10 @@ DECLARE
 	max_val INT := 0;
 	val float := 0;
 BEGIN
+
+	DBMS_OUTPUT.PUT_LINE('Min Max Scaler / Normalization');
+	
+	
 	select MIN(numerical_col_1) into min_val from raw_data;
 	select MAX(numerical_col_1) into max_val from raw_data;
 	DBMS_OUTPUT.PUT_LINE( min_val || ' ' || max_val);
@@ -258,9 +289,11 @@ END;
 /
 
 commit;
-select * from raw_data;
+
+SELECT id, numerical_col_1 FROM raw_data;
 
 
+SELECT id, numerical_col_2 FROM raw_data;
 
 DECLARE
 	min_val INT;
@@ -270,11 +303,15 @@ DECLARE
 	temp int ;
 	l_str varchar2(100);
 BEGIN
+
+	
+	DBMS_OUTPUT.PUT_LINE('Binning');	
+	
 	SELECT MIN(numerical_col_2) INTO min_val FROM raw_data;
 	SELECT MAX(numerical_col_2) INTO max_val FROM raw_data;
 	DBMS_OUTPUT.PUT_LINE('Min and Max value for numerical_col_2');
 	DBMS_OUTPUT.PUT_LINE(min_val || ' ' || max_val);
-	increment_val := (max_val - min_val) / 8;
+	increment_val := (max_val - min_val) / no_of_divisions;
 	DBMS_OUTPUT.PUT_LINE('Increment Value : ' || increment_val);
 	FOR r IN (SELECT id,numerical_col_2 FROM RAW_DATA) 
 		LOOP					
@@ -290,13 +327,63 @@ END;
 /
 
 commit;
-select * from raw_data;
+
+SELECT id, numerical_col_2 FROM raw_data;
+
+
+SELECT id, categorical_col_2 FROM raw_data;
+
+DECLARE
+	create_column_command varchar2(100);
+	first_part varchar2(100);
+	counter int := 1;
+	update_col_command varchar2(200);
+BEGIN
+	
+
+	DBMS_OUTPUT.PUT_LINE('Discretization of categorical Data');	
+	
+	FOR r IN (SELECT categorical_col_2 FROM (SELECT DISTINCT categorical_col_2 FROM RAW_DATA)) 
+		LOOP										
+			create_column_command := 'ALTER TABLE RAW_DATA ADD col_';
+			create_column_command := CONCAT(create_column_command, r.categorical_col_2);
+			create_column_command := CONCAT(create_column_command, ' number default 0');
+			counter := counter + 1;
+			--DBMS_OUTPUT.PUT_LINE(create_column_command);
+			execute immediate create_column_command;
+			
+	END LOOP;
+	
+	
+	
+	FOR r IN (SELECT categorical_col_2 FROM (SELECT DISTINCT categorical_col_2 FROM RAW_DATA)) 
+		LOOP										
+			
+			FOR x IN (SELECT id,categorical_col_2 FROM RAW_DATA where categorical_col_2 = r.categorical_col_2)
+			LOOP					
+				update_col_command := 'UPDATE raw_data SET col_';
+				update_col_command := CONCAT(update_col_command, r.categorical_col_2);
+				update_col_command := CONCAT(update_col_command, ' = 1 WHERE id = ');
+				update_col_command := CONCAT(update_col_command, TO_CHAR(x.id));
+				--DBMS_OUTPUT.PUT_LINE(update_col_command);
+				execute immediate update_col_command;
+			END LOOP;
+			
+	END LOOP;
+	
+			
+	
+END;
+/
+
+commit;
+
+SELECT * FROM raw_data;
 
 
 
-
-
-
+TODO
+-- encoding
 
 
 
